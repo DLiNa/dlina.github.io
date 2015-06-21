0c93230347d8c69367192d81c0af031262479373
mathias-goebel <goebel@sub.uni-goettingen.de>
2015-06-21 15:19:09 +0200

    minor changes

--- quick-deb.sh
+++ quick-deb.sh
@@ -3,17 +3,24 @@
 # without generating more than 2000 pages.
 # requires ruby, gem github-pages
 
-cd /tmp
-rm -rf dlina.github.io
-git clone https://github.com/dlina/dlina.github.io.git
+rm -rf /tmp/dlina.github.io
+if [ "$1"  == "remote" ];
+	then
+		cd /tmp
+		git clone https://github.com/dlina/dlina.github.io.git
+	else
+		cd ..
+		cp -r dlina.github.io /tmp/
+		cd /tmp
+fi
 cd dlina.github.io
-for i in `seq 0 7`; 
-	do 
+for i in `seq 0 7`;
+	do
 		rm -f networks/_posts/*$i.md;
 		for j in `seq 0 9`;
 			do
-				rm -f networks/_posts/*$j$i.md;
+				rm -f networks/_posts/*$i$j.md;
 			done
-		 
 	done
 bundle exec jekyll serve --baseurl ''
+exit 0
