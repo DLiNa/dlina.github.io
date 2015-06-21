4e9430070e17b6a3d624a9fae6a42faf64c7707a
mathias-goebel <goebel@sub.uni-goettingen.de>
2015-06-21 14:42:04 +0200

    minor changes on style an usability

--- quick-deb.sh
+++ quick-deb.sh
@@ -4,10 +4,16 @@
 # requires ruby, gem github-pages
 
 cd /tmp
+rm -rf dlina.github.io
 git clone https://github.com/dlina/dlina.github.io.git
 cd dlina.github.io
 for i in `seq 0 7`; 
 	do 
-		rm -f networks/_posts/*$i.md 
+		rm -f networks/_posts/*$i.md;
+		for j in `seq 0 9`;
+			do
+				rm -f networks/_posts/*$j$i.md;
+			done
+		 
 	done
 bundle exec jekyll serve --baseurl ''
